fca02fc9a9fa6677e6aada52d3c6d5a4bbbc4e85
Paul <paul@paul-mayer.eu>
2020-11-22 21:25:51 +0100

    test wasm rust

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ use std::cell::Cell;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-
+use serde::{Deserialize, Serialize};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
 
 #[wasm_bindgen]
@@ -15,20 +17,57 @@ extern "C" {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
-
+/*
 #[wasm_bindgen]
 pub fn load(name: &str) {
     let img_bytes = reqwest::blocking::get("https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png")?
     .bytes()?;
     let image = image::load_from_memory(&img_bytes)?;
 }
-
+*/
 //pub fn loadImage() -> {
     /*l
     et img_bytes = reqwest::blocking::get("...")?
     .bytes()?;
     let image = image::load_from_memory(&img_bytes)?;
     */   
+
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct Content {
+        pub value: String,
+    }
+    
+    #[wasm_bindgen]
+    pub async fn run() -> Result<JsValue, JsValue> {
+        let mut opts = RequestInit::new();
+        opts.method("GET");
+        opts.mode(RequestMode::Cors);
+    
+        let url = format!("https://hub.dummyapis.com/singlelist?text=Test&noofRecords=10");
+    
+        let request = Request::new_with_str_and_init(&url, &opts)?;
+    
+        /*request
+            .headers()
+            .set("Accept", "application/vnd.github.v3+json")?;*/
+
+        let window = web_sys::window().unwrap();
+        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    
+        // `resp_value` is a `Response` object.
+        assert!(resp_value.is_instance_of::<Response>());
+        let resp: Response = resp_value.dyn_into().unwrap();
+    
+        // Convert this other `Promise` into a rust `Future`.
+        let json = JsFuture::from(resp.json()?).await?;
+    
+        // Use serde to parse the JSON into a struct.
+        let branch_info: Branch = json.into_serde().unwrap();
+    
+        // Send the `Branch` struct back to JS as an `Object`.
+        Ok(JsValue::from_serde(&branch_info).unwrap())
+    }
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
